1a13d163614470d76a2d54199c852306080fe872
mjsadeghinia <mjsadeghinia@outlook.com>
2024-06-30 20:10:08 +0000

    for backup

--- pv_data.py
+++ pv_data.py
@@ -40,11 +40,12 @@ def divide_pv_data(pres,vols, t_interval = 175):
     # Dividing the data into different curves
     pres_divided = []
     vols_divided = []
-    
-    num_cycles = int(len(pres)/t_interval)
-    for k in range(num_cycles):
-        pres_divided.append(pres[k*t_interval:(k+1)*t_interval])
-        vols_divided.append(vols[k*t_interval:(k+1)*t_interval])
+    peaks, _ = find_peaks(pres,distance=150)
+
+    num_cycles = int(len(peaks))
+    for i in range(num_cycles-1):
+        pres_divided.append(pres[peaks[i]:peaks[i+1]])
+        vols_divided.append(vols[peaks[i]:peaks[i+1]])
     
     return pres_divided, vols_divided
 
@@ -54,6 +55,8 @@ def slice_divided_data(pres_divided, vols_divided, offset = 50):
     vols_divided_sliced = []
     pres_divided_sliced = []
     for n, (vols, pres) in enumerate(zip(vols_divided, pres_divided)):
+        pres = pres / np.max(pres)
+        vols = vols / np.max(vols)
         dpres = pres - pres[0]
         dvols = vols - vols[0] 
         dist = np.sqrt((dpres[offset:]) ** 2 + (dvols[offset:]) ** 2)
@@ -64,12 +67,43 @@ def slice_divided_data(pres_divided, vols_divided, offset = 50):
     
     return pres_divided_sliced, vols_divided_sliced
 
+def smooth_data(pres_divided_sliced,vols_divided_sliced, smooth_level = 1):
+    pres_smooth = []
+    vols_smooth = []
+    for vols, pres in zip(vols_divided_sliced, pres_divided_sliced):
+        vols = np.append(vols, vols[0])
+        pres = np.append(pres, pres[0])
+        tck, _ = splprep([vols, pres], s=smooth_level, per=True)
+        # Evaluate the B-spline
+        unew = np.linspace(0, 1.0, 1000)
+        data = splev(unew, tck)
+        vols_smooth.append(data[0])
+        pres_smooth.append(data[1])
+    return pres_smooth, vols_smooth
+
+def plot_data(pres, vols):
+    for i in range(len(pres)):
+        plt.figure()
+        plt.plot(vols[i],pres[i])
 # %%P
 pres, vols = load_pv_data(pv_data_dir)
+pres_divided, vols_divided = divide_pv_data(pres, vols)
+pres_divided_sliced, vols_divided_sliced = slice_divided_data(pres_divided, vols_divided)
+res_smooth, vols_smooth = smooth_data(pres_divided_sliced,vols_divided_sliced)
 
 
+
+#%%
+k = 29
+plt.figure()
+plt.plot(vols_divided_sliced[k],pres_divided_sliced[k])
+plt.plot(vols_divided[k],pres_divided[k])
+
+n=29
+pres = pres_divided[n]
+vols = vols_divided[n]
+#%%
 pres = pres[:1000]
-peaks, _ = find_peaks(pres,distance=150)
 plt.plot(pres)
 plt.plot(peaks, pres[peaks], "x")
 plt.plot(np.zeros_like(pres), "--", color="gray")
@@ -82,6 +116,7 @@ pres_divided_sliced, vols_divided_sliced = slice_divided_data(pres_divided, vols
 
 k =29
 plt.plot(vols_divided[k],pres_divided[k])
+k= 0
 plt.plot(vols_divided_sliced[k],pres_divided_sliced[k])
 #%%
 
