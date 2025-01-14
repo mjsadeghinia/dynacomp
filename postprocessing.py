336e73ce0a17acd203a6ab6d21d29aa406d59370
mjsadeghinia <mjsadeghinia@outlook.com>
2025-01-14 11:10:58 +0000

    added normalization and averaging

--- postprocessing.py
+++ postprocessing.py
@@ -2,7 +2,8 @@
 import numpy as np
 from pathlib import Path
 from structlog import get_logger
-from collections import defaultdict
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 
 import json
 import argparse
@@ -27,22 +28,23 @@ def initialize_results_dict(group_list, time_list, diameter_list):
     return results_dict
 
 
-def get_time_data(sample_dir, pv_folder = 'PV Data'):
-    data_path  = sample_dir / pv_folder / pv_folder
+def get_time_data(sample_dir, pv_folder="PV Data"):
+    data_path = sample_dir / pv_folder / pv_folder
     csv_files = list(data_path.glob("*.csv"))
     if len(csv_files) != 1:
         logger.error("Data folder must contain exactly 1 .mat file.")
         return
     pv_data = np.loadtxt(csv_files[0], delimiter=",", skiprows=1)
-    return pv_data[:,0]
+    return pv_data[:, 0]
 
-def parse_sample_data(settings_fname, output_folder):
+
+def parse_sample_data(settings_fname, results_folder):
     with open(settings_fname, "r") as file:
         settings = json.load(file)
 
     sample_name = settings_fname.stem
     sample_dir = Path(settings["path"])
-    data_dir = sample_dir / output_folder / "00_Modeling"
+    data_dir = sample_dir / results_folder / "00_Modeling"
     result_path = data_dir / "results_data.csv"
 
     if not result_path.exists():
@@ -52,10 +54,114 @@ def parse_sample_data(settings_fname, output_folder):
     sample_data = np.loadtxt(result_path, delimiter=",", skiprows=1)
     time_data = get_time_data(sample_dir)
     # append two additional timing for unloading and loading to ED
-    time_data = np.append([0,0],time_data)
-    sample_data[:,0] = time_data
-    
+    time_data = np.append([0, 0], time_data)
+    sample_data[:, 0] = time_data
+
     return sample_name, settings, sample_data
+
+
+def normalize_and_interpolate(times_dict, data_dict, N=200):
+    """
+    Normalize time to range [0, 1] and interpolate data for N points.
+
+    Parameters:
+    - times_dict (dict): Dictionary containing times corresponding to the data.
+    - data_dict (dict): Dictionary containing volumes or pressures.
+    - N (int): Number of equally spaced points for interpolation. Default is 200.
+
+    Returns:
+    - interpolated_dict (dict): New dictionary with interpolated values.
+    - normalized_times_dict (dict): New dictionary with normalized time values.
+    """
+    interpolated_dict = {}
+    normalized_times_dict = {}
+
+    for group, times_group in times_dict.items():
+        interpolated_dict[group] = {}
+        normalized_times_dict[group] = {}
+        for time_key, times_list in times_group.items():
+            if isinstance(times_list, dict):  # Check if there are diameters
+                interpolated_dict[group][time_key] = {}
+                normalized_times_dict[group][time_key] = {}
+                for diameter, times in times_list.items():
+                    interpolated_dict[group][time_key][diameter] = []
+                    normalized_times_dict[group][time_key][diameter] = []
+                    for time_series, data_series in zip(
+                        times, data_dict[group][time_key][diameter]
+                    ):
+                        # Normalize time to [0, 1]
+                        normalized_time = (time_series - time_series.min()) / (
+                            time_series.max() - time_series.min()
+                        )
+
+                        # Interpolate data
+                        interpolator = interp1d(
+                            normalized_time, data_series, kind="linear"
+                        )
+                        new_time = np.linspace(0, 1, N)
+                        new_values = interpolator(new_time)
+
+                        interpolated_dict[group][time_key][diameter].append(new_values)
+                        normalized_times_dict[group][time_key][diameter].append(
+                            new_time
+                        )
+            else:  # Handle case without diameters
+                interpolated_dict[group][time_key] = []
+                normalized_times_dict[group][time_key] = []
+                for time_series, data_series in zip(
+                    times_list, data_dict[group][time_key]
+                ):
+                    # Normalize time to [0, 1]
+                    normalized_time = (time_series - time_series.min()) / (
+                        time_series.max() - time_series.min()
+                    )
+
+                    # Interpolate data
+                    interpolator = interp1d(normalized_time, data_series, kind="linear")
+                    new_time = np.linspace(0, 1, N)
+                    new_values = interpolator(new_time)
+
+                    interpolated_dict[group][time_key].append(new_values)
+                    normalized_times_dict[group][time_key].append(new_time)
+
+    return interpolated_dict, normalized_times_dict
+
+
+def calculate_data_average_and_std(data_dict):
+    """
+    Calculate the average and standard deviation of all data along the list for each group, time_key, and diameter (if exists).
+
+    Parameters:
+    - data_dict (dict): Dictionary containing data to be averaged.
+
+    Returns:
+    - averaged_dict (dict): Dictionary with keys as "group_time_key_diameter" and averaged data as values.
+    - std_dict (dict): Dictionary with keys as "group_time_key_diameter" and standard deviations as values.
+    """
+    averaged_dict = {}
+    std_dict = {}
+
+    for group, times_group in data_dict.items():
+        for time_key, times_list in times_group.items():
+            if isinstance(times_list, dict):  # Check if there are diameters
+                for diameter, data_list in times_list.items():
+                    key = f"{group}_{time_key}_{diameter}"
+                    if data_list:
+                        averaged_dict[key] = np.mean(data_list, axis=0)
+                        std_dict[key] = np.std(data_list, axis=0)
+                    else:
+                        averaged_dict[key] = None
+                        std_dict[key] = None
+            else:  # Handle case without diameters
+                key = f"{group}_{time_key}"
+                if times_list:
+                    averaged_dict[key] = np.mean(times_list, axis=0)
+                    std_dict[key] = np.std(times_list, axis=0)
+                else:
+                    averaged_dict[key] = None
+                    std_dict[key] = None
+
+    return averaged_dict, std_dict
 
 
 # %%
@@ -70,14 +176,26 @@ def main(args=None) -> int:
     )
 
     parser.add_argument(
-        "-o",
-        "--output_folder",
+        "-r",
+        "--results_folder",
         default="t3",
         type=str,
         help="The result folder name tha would be created in the directory of the sample.",
     )
 
+    parser.add_argument(
+        "-o",
+        "--output_folder",
+        default="/home/shared/00_results_average",
+        type=str,
+        help="The result folder name tha would be created in the directory of the sample.",
+    )
+
     args = parser.parse_args(args)
+
+    setting_dir = Path(args.settings_dir)
+    results_folder = args.results_folder
+    output_folder = args.output_folder
 
     # Initialize the results dicts
     group_list = ["SHAM", "AS"]
@@ -86,17 +204,16 @@ def main(args=None) -> int:
 
     ids = initialize_results_dict(group_list, time_list, diameter_list)
     times = initialize_results_dict(group_list, time_list, diameter_list)
+    activations = initialize_results_dict(group_list, time_list, diameter_list)
     volumes = initialize_results_dict(group_list, time_list, diameter_list)
     pressures = initialize_results_dict(group_list, time_list, diameter_list)
 
-    setting_dir = Path(args.settings_dir)
-    output_folder = args.output_folder
     for settings_fname in sorted(setting_dir.iterdir()):
         if not settings_fname.suffix == ".json":
             continue
 
         sample_name, settings, sample_data = parse_sample_data(
-            settings_fname, output_folder
+            settings_fname, results_folder
         )
         if sample_data is None:
             continue
@@ -108,15 +225,25 @@ def main(args=None) -> int:
         if diameter is None:
             ids[group][time].append(sample_name)
             times[group][time].append(sample_data[:, 0])
-            pressures[group][time].append(sample_data[:, 1])
+            activations[group][time].append(sample_data[:, 1])
             volumes[group][time].append(sample_data[:, 2])
+            pressures[group][time].append(sample_data[:, 4])
         else:
             ids[group][time][diameter].append(sample_name)
             times[group][time][diameter].append(sample_data[:, 0])
-            pressures[group][time][diameter].append(sample_data[:, 1])
+            activations[group][time][diameter].append(sample_data[:, 1])
             volumes[group][time][diameter].append(sample_data[:, 2])
+            pressures[group][time][diameter].append(sample_data[:, 4])
 
-    breakpoint()
+    interpolated_volumes, normalized_times = normalize_and_interpolate(times, volumes)
+    interpolated_pressures, _ = normalize_and_interpolate(times, pressures)
+    interpolated_actvations, _ = normalize_and_interpolate(times, pressures)
+
+    averaged_actvations, std_actvations = calculate_data_average_and_std(interpolated_actvations)
+    averaged_volumes, std_volumes = calculate_data_average_and_std(interpolated_volumes)
+    averaged_pressures, std_pressures = calculate_data_average_and_std(interpolated_pressures)
+    normalized_times, _ = calculate_data_average_and_std(normalized_times)
+
 if __name__ == "__main__":
     main()
 # %%
