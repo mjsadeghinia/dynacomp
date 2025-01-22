2ad211d2351a11bc79337afd47a2b0f842a4372c
mjsadeghinia <mjsadeghinia@outlook.com>
2025-01-22 12:32:26 +0000

    fixed the unloading bug, removed prestiffening

--- all_pipeline.py
+++ all_pipeline.py
@@ -20,7 +20,7 @@ def get_sample_name(sample_num, setting_dir):
 number_i = 1
 number_f = 56
 mesh_quality = "coarse"
-output_folder = "t3"
+output_folder = "coarse_mesh"
 cpus_num = 8
 setting_dir = Path("/home/shared/dynacomp/settings")
 sample_with_error = []
