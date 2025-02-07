231f93167bf7839258e661df37a0cac6fef267a6
mjsadeghinia <mjsadeghinia@outlook.com>
2025-02-07 15:01:00 +0000

    added activation comparison

---
+++ compare_results.py
@@ -0,0 +1,101 @@
+import numpy as np
+from pathlib import Path
+from structlog import get_logger
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+import dolfin
+import pulse
+import utils_post
+
+import logging
+import argparse
+
+
+logger = get_logger()
+
+
+# %%# UNITS:
+# [kg]   [mm]    [s]    [mN]     [kPa]       [mN-mm]	    g = 9.806e+03
+def get_time_data(data_dir, pv_folder="PV Data"):
+    data_path = data_dir.parent / pv_folder / pv_folder
+    csv_files = list(data_path.glob("*.csv"))
+    csv_file = csv_files[0]
+    if len(csv_files) != 1:
+        n = [n for n, address in enumerate(csv_files) if "RAW" not in address.name]
+        csv_file = csv_files[n[0]]
+        logger.warning(f"There are {len(csv_files)} .csv files, we are using {csv_file.name}.")
+
+    pv_data = np.loadtxt(csv_file, delimiter=",", skiprows=1)
+    return pv_data[:, 0]
+
+def load_data(dir):
+    data_dir = Path(dir)
+    result_path = data_dir / "00_Modeling" / "results_data.csv"
+    if not result_path.exists():
+        logger.warning(f"Results do not exist for {dir}")
+        return None
+    sample_data = np.loadtxt(result_path, delimiter=",", skiprows=1)
+    time_data = get_time_data(data_dir)
+    # append two additional timing for unloading and loading to ED
+    time_data = np.append([0, 0], time_data)
+    sample_data[:, 0] = time_data
+    return sample_data
+
+def normalize_data(data, N=100):
+    normalize_data = np.zeros((N,data.shape[1]))
+    # Normalize time to [0, 1]
+    time_series = data[:,0]
+    normalized_time = (time_series - time_series.min()) / (time_series.max() - time_series.min())
+    # Interpolate data
+    for k in range(data.shape[1]-1): 
+        interpolator = interp1d(normalized_time, data[:, k+1], kind="linear")
+        new_time = np.linspace(0, 1, N)
+        new_values = interpolator(new_time)
+        normalize_data[:, k+1] = new_values
+    normalize_data[:,0] = np.linspace(0, 1, N)
+
+    return normalize_data
+
+def main(args=None) -> int:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--first",
+        default='/home/shared/dynacomp/00_data/CineData/AS/6weeks/130/OP126_1/coarse_mesh_v2',
+        type=Path,
+        help="The address to the sample 1.",
+    )
+
+    parser.add_argument(
+        "--second",
+        default='/home/shared/dynacomp/00_data/CineData/AS/6weeks/130/OP126_3/coarse_mesh_v2',
+        type=Path,
+        help="The address to the sample 2.",
+    )
+
+    args = parser.parse_args(args)
+
+    data_1 = load_data(args.first)
+    data_2 = load_data(args.second)
+    
+    normalize_data_1 = normalize_data(data_1)
+    normalize_data_2 = normalize_data(data_2)
+
+    fig, ax = plt.subplots()
+    ax.plot(normalize_data_1[:,0], normalize_data_1[:,1], color = 'r', label = args.first.parent.stem)
+    ax.plot(normalize_data_2[:,0], normalize_data_2[:,1], color = 'b', label = args.second.parent.stem)
+    ax.set_xlim(0, 1)
+    ax.set_ylim(-10,100)
+    ax.set_ylabel('Activation [kPa]')
+    ax.set_xlabel("Normalized Time [-]")
+    ax.grid()
+    ax.legend()
+    
+    fname = f'Activation_comparison'
+    fig.savefig(fname, dpi=300)
+    plt.close(fig)
+
+
+    
+if __name__ == "__main__":
+    main()
