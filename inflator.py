d6630387659240433315c9d6d95f1b25500d7a8c
mjsadeghinia <mjsadeghinia@outlook.com>
2025-05-07 09:27:57 +0000

    refactoring and saving plot as solving the inflations

--- inflator.py
+++ inflator.py
@@ -1,229 +1,164 @@
+# main.py
 import argparse
-import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 import json
-import scipy.stats
 
 import arg_parser
 import pulse
 import dolfin
 from heart_model import HeartModelDynaComp
-from datacollector import DataCollector_Inflator
+from datacollector import DataCollectorInflator
 
-from structlog import get_logger
+def load_settings(settings_dir: Path, sample_num: int) -> dict:
+    files = sorted([f for f in settings_dir.iterdir() if f.suffix == ".json"])
+    with open(files[sample_num - 1], 'r') as f:
+        return json.load(f)
 
-logger = get_logger()
-comm = dolfin.MPI.comm_world
-
-
-# %%
-def load_settings(settings_dir, sample_num):
-    sorted_files = sorted([file for file in settings_dir.iterdir() if file.is_file() and file.suffix == ".json"])
-    settings_fname = sorted_files[sample_num - 1]
-    with open(settings_fname, "r") as file:
-        settings = json.load(file)
-    return settings
-
-
-def load_pv_data(pvcalibration_data_dir):
-    # These are the paramters we are using: mri_time, regirstered_pressures, regirstered_calibrated_volumes
-    fname = pvcalibration_data_dir / "calibrated_pv_data.csv"
-    PV_data = np.loadtxt(fname, delimiter=",")
-    time, pres_mmHg, vols = PV_data[:, 0], PV_data[:, 1], PV_data[:, 2]
-    # Convert mmHg to kPa
-    mmHg_to_kPa = 0.133322
-    pres = pres_mmHg * mmHg_to_kPa
+def load_pv_data(directory: Path):
+    data = np.loadtxt(directory / "calibrated_pv_data.csv", delimiter=',')
+    time, pres_mmHg, vols = data.T
+    pres = pres_mmHg * 0.133322  # mmHg to kPa
     return time, pres, vols
 
+def load_edpvr(directory: Path):
+    path = next(f for f in directory.iterdir() if "EDPVR.csv" in f.name)
+    data = np.loadtxt(path, delimiter=',')
+    pres = data[:, 0] * 0.133322
+    vols = data[:, 1]
+    return pres, vols
 
-def load_edpvr(data_dir):
-    PV_data_fname = [fname for fname in data_dir.iterdir() if "EDPVR.csv" in fname.as_posix()][0]
-    PV_data = np.loadtxt(PV_data_fname.as_posix(), delimiter=",")
-    mmHg_to_kPa = 0.133322
-    pressures = PV_data[:, 0] * mmHg_to_kPa
-    volumes = PV_data[:, 1]
-    return pressures, volumes
-
-
-# %%
-def main(args=None) -> int:
-    """
-    Parse the command-line arguments.
-    """
+def main():
     parser = argparse.ArgumentParser()
-
     parser.add_argument(
-        "-n",
-        "--number",
-        nargs="+",
+        '-n',
+        '--number',
+        nargs='*',
         type=int,
-        help="The sample number(s), will process all the sample if not indicated",
+        default=None,
+        help='Sample number(s) to process. If omitted, all samples in settings_dir will be processed.'
     )
-
     parser.add_argument(
-        "--settings_dir",
-        default="/home/shared/dynacomp/settings",
+        '--settings_dir',
         type=Path,
-        help="The settings directory where json files are stored.",
+        default=Path('/home/shared/dynacomp/settings'),
+        help='Directory where JSON settings files are stored.'
     )
-
     parser.add_argument(
-        "-d",
-        "--data_dir",
-        default="/home/shared/00_data",
+        '-d',
+        '--data_dir',
         type=Path,
-        help="The settings directory where data files are stored.",
+        default=Path('/home/shared/00_data'),
+        help='Directory where data files are stored.'
     )
-
     parser.add_argument(
-        "-s",
-        "--scan_type",
-        default="TPM",
+        '-s',
+        '--scan_type',
         type=str,
-        help="The scan type. Settings will be loaded accordingly from json file",
+        default='TPM',
+        help='Scan type; subdirectories will be named accordingly.'
     )
-
     parser.add_argument(
-        "-r",
-        "--results_dir",
-        default="/home/shared/01_results_coarse_mesh",
+        '-r',
+        '--results_dir',
         type=Path,
-        help="The results folder where the processed data should be saved.",
+        default=Path('/home/shared/01_results_coarse_mesh'),
+        help='Directory where results will be saved.'
     )
-
     parser.add_argument(
-        "--pressure_multiplier",
-        default=2,
+        '--pressure_multiplier',
         type=float,
-        help="The multiplier for the pressure to be used in the inflation simulation.",
+        default=2.0,
+        help='Multiplier for the initial pressure step.'
     )
-
     parser.add_argument(
-        "--pressure_steps",
-        default=20,
+        '--pressure_steps',
         type=int,
-        help="The number of pressure steps to be used in the inflation simulation.",
-    )
-
-    # Arguments for HeartModel boundary conditions
-    parser.add_argument(
-        "--pericardium_spring",
-        default=0.0001,
-        type=float,
-        help="HeartModel BC: The stiffness of the spring on the pericardium.",
+        default=20,
+        help='Number of pressure increments in the inflation simulation.'
     )
     parser.add_argument(
-        "--base_spring",
-        default=1,
+        '--pericardium_spring',
         type=float,
-        help="HeartModel BC: The stiffness of the spring at the base.",
+        default=1e-4,
+        help='Spring stiffness for pericardium boundary condition.'
     )
+    parser.add_argument(
+        '--base_spring',
+        type=float,
+        default=1.0,
+        help='Spring stiffness at the heart base boundary condition.'
+    )
+    args = parser.parse_args()
 
-    args = parser.parse_args(args)
+    # Determine which samples to process
+    if args.number:
+        sample_list = args.number
+    else:
+        settings_files = sorted([f for f in args.settings_dir.iterdir() if f.suffix == ".json"])
+        sample_list = list(range(1, len(settings_files) + 1))
 
-    sample_nums = args.number
-    settings_dir = args.settings_dir
-    data_dir = args.data_dir
-    results_dir = args.results_dir
-    scan_type = args.scan_type
     bc_params = arg_parser.create_bc_params(args)
-    pressure_multiplier = args.pressure_multiplier
-    pressure_steps = args.pressure_steps
+    comm = dolfin.MPI.comm_world
 
-    for sample_num in sample_nums:
-        settings = load_settings(settings_dir, sample_num)
-        sample_name = settings["id"]
-        pv_data_dir = results_dir / sample_name / "PV Data"
-        experiment_data_dir = results_dir / sample_name / scan_type
-        pvcalibration_data_dir = results_dir / sample_name / scan_type / "01_PVCalibration"
-        unloading_data_dir = experiment_data_dir / "02_Unloading"
-        modeling_outdir = experiment_data_dir / "03_Modeling"
+    for sample in sample_list:
+        settings = load_settings(args.settings_dir, sample)
+        name = settings['id']
+        out_dirs = {
+            'pv': args.results_dir / name / 'PV Data',
+            'calib': args.results_dir / name / args.scan_type / '01_PVCalibration',
+            'unload': args.results_dir / name / args.scan_type / '02_Unloading',
+            'model': args.results_dir / name / args.scan_type / '03_Modeling'
+        }
         if comm.rank == 0:
-            modeling_outdir = arg_parser.prepare_oudir_processing(modeling_outdir, comm)
+            arg_parser.prepare_oudir_processing(out_dirs['model'], comm)
         comm.Barrier()
 
-        time, pres, vols = load_pv_data(pvcalibration_data_dir)
-        edpvr_pres, edpvr_vols_uncalibrated = load_edpvr(pv_data_dir)
-        edpvr_vols = settings["PV"]["calibration"]["a"] * edpvr_vols_uncalibrated + settings["PV"]["calibration"]["b"]
+        # Load data
+        time, pres, vols = load_pv_data(out_dirs['calib'])
+        edpvr_pres, edpvr_vols_unc = load_edpvr(out_dirs['pv'])
+        a = settings['PV']['calibration']['a']
+        b = settings['PV']['calibration']['b']
+        edpvr_vols = a * edpvr_vols_unc + b
 
-        unloaded_geometry_fname = unloading_data_dir / "unloaded_geometry_0_with_fibers.h5"
-        unloaded_geometry = pulse.HeartGeometry.from_file(unloaded_geometry_fname.as_posix(), comm=comm)
-        heart_model = HeartModelDynaComp(
-            geo=unloaded_geometry,
+        # Set up model
+        geometry = pulse.HeartGeometry.from_file(
+            (out_dirs['unload'] / 'unloaded_geometry_0_with_fibers.h5').as_posix(), comm=comm
+        )
+        model = HeartModelDynaComp(
+            geo=geometry,
             bc_params=bc_params,
-            matparams=settings["matparams"],
+            matparams=settings['matparams'],
             comm=comm,
         )
-        collector = DataCollector_Inflator(outdir=modeling_outdir, problem=heart_model)
-        # Initializing the model
-        v = heart_model.compute_volume(activation_value=0, pressure_value=0)
-        collector.collect(
-            time=0,
-            pressure=0,
-            volume=v,
+
+        # Initialize data collector with live plotting
+        collector = DataCollectorInflator(
+            outdir=out_dirs['model'],
+            problem=model,
+            pv_vols=vols,
+            pv_pres=pres,
+            edpvr_vols=edpvr_vols,
+            edpvr_pres=edpvr_pres,
+            live_plot=True
         )
-        start_time = 1
-        # Set the pressure to be used in the inflation simulation
-        loading_pressures = np.linspace(0, pres[0] * pressure_multiplier, pressure_steps)
-        for i, p in enumerate(loading_pressures):
-            v = heart_model.compute_volume(activation_value=0, pressure_value=p)
-            p_current = heart_model.get_pressure()
-            v_current = heart_model.get_volume()
-            collector.collect(
-                time=i + start_time,
-                pressure=p_current,
-                volume=v_current,
-            )
 
+        # Initial state
+        v0 = model.compute_volume(activation_value=0, pressure_value=0)
+        collector.collect(time=0, pressure=0, volume=v0)
+
+        # Simulation loop
+        pressures = np.linspace(0, pres[0] * args.pressure_multiplier, args.pressure_steps)
+        for i, p in enumerate(pressures, start=1):
+            model.compute_volume(activation_value=0, pressure_value=p)
+            collector.collect(time=i, pressure=model.get_pressure(), volume=model.get_volume())
+
+        # Finalize plot session
         if comm.rank == 0:
-            # Calculate the x value at which y = 0 using the regression line equation (avoid division by zero)
-            res = scipy.stats.linregress(edpvr_vols, edpvr_pres)
-            v_0 = -res.intercept / res.slope if res.slope != 0 else float("nan")
-            # Calculate the standard error of the slope and intercept
-            tinv = lambda p, df: abs(scipy.stats.t.ppf(p / 2, df))
-            ts = tinv(0.05, len(edpvr_vols) - 2)
-            # Plotting the data
-            fig, ax = plt.subplots(figsize=(8, 6))
-            ax.plot(vols, pres, "k", linewidth=1)
-            ax.scatter(vols, pres, s=15, c="k", label="PV Data")
-            ax.scatter(edpvr_vols, edpvr_pres, s=8, c="r", label="EDPVR")
-            ax.plot(collector.volumes, collector.pressures, "g", linewidth=1)
-            ax.scatter(collector.volumes, collector.pressures, color="g", s=8, label="Simulation")
+            collector.finalize_plot()
 
-            plt.xlabel("Volume [micro Liter]")
-            plt.ylabel("LV Pressure [mmHg]")
-
-            # Add a title with the slope and intercept
-            textstr = (
-                f"slope (95%): {res.slope:.3f} $\pm$ {ts*res.stderr:.3f}\n"
-                f"$v_0$ (P=0): {v_0:.2f}\n"
-                f"$v_0 estimated$ (P=0): {collector.volumes[0]:.2f}"
-            )
-            ax.text(
-                0.05,
-                0.95,
-                textstr,
-                transform=ax.transAxes,
-                fontsize=10,
-                verticalalignment="top",
-                # bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-            )
-            ax.plot(edpvr_vols, res.intercept + res.slope * edpvr_vols, "b")
-            ax.axhline(y=0, color="gray", linestyle="--")
-
-            # Add a second y-axis for LV Pressure in kPa
-            ax2 = ax.twinx()
-            mmHg_to_kPa = 0.133322
-            ymin, ymax = ax.get_ylim()
-            ax2.set_ylim(ymin * mmHg_to_kPa, ymax * mmHg_to_kPa)
-            ax2.set_ylabel("LV Pressure [kPa]")
-
-            ax.legend(loc="lower left")
-            fname = modeling_outdir / f"inflation_results.png"
-            plt.savefig(fname, dpi=300)
-            plt.close()
+    return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
