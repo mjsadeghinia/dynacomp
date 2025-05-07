3e9632cd243fd021e217c45a440e8305434a057f
mjsadeghinia <mjsadeghinia@outlook.com>
2025-05-07 10:18:39 +0000

    added run_inflator function to be used for other modules

--- inflator.py
+++ inflator.py
@@ -28,6 +28,99 @@ def load_edpvr(directory: Path):
     vols = data[:, 1]
     return pres, vols
 
+def run_inflator(
+    sample: int,
+    settings_dir: Path,
+    results_dir: Path,
+    scan_type: str = 'TPM',
+    pressure_multiplier: float = 2.0,
+    pressure_steps: int = 20,
+    pericardium_spring: float = 1e-4,
+    base_spring: float = 1.0,
+    live_plot: bool = True
+) -> DataCollectorInflator:
+    """
+    Run an inflation simulation for a given sample index using explicit parameters.
+
+    Parameters:
+        sample: sample number (1-based index into sorted settings_dir JSON files)
+        settings_dir: directory containing JSON settings files
+        results_dir: base output directory for results
+        scan_type: name of scan-type subdirectory
+        pressure_multiplier: multiplier for initial pressure
+        pressure_steps: number of pressure increments
+        pericardium_spring: pericardium BC stiffness
+        base_spring: base BC stiffness
+        live_plot: whether to show and update a live plot
+
+    Returns:
+        DataCollectorInflator with collected data and saved figures
+    """
+    # Load settings
+    settings = load_settings(settings_dir, sample)
+    sample_id = settings['id']
+
+    # Prepare directories
+    out_dirs = {
+        'pv': results_dir / sample_id / 'PV Data',
+        'calib': results_dir / sample_id / scan_type / '01_PVCalibration',
+        'unload': results_dir / sample_id / scan_type / '02_Unloading',
+        'model': results_dir / sample_id / scan_type / '03_Modeling'
+    }
+    comm = dolfin.MPI.comm_world
+    if comm.rank == 0:
+        arg_parser.prepare_oudir_processing(out_dirs['model'], comm)
+    comm.Barrier()
+
+    # Load PV and EDPVR data
+    time, pres, vols = load_pv_data(out_dirs['calib'])
+    edpvr_pres, edpvr_vols_unc = load_edpvr(out_dirs['pv'])
+    a = settings['PV']['calibration']['a']
+    b = settings['PV']['calibration']['b']
+    edpvr_vols = a * edpvr_vols_unc + b
+
+    # Initialize heart model
+    bc_params = arg_parser.create_bc_params(
+        argparse.Namespace(
+            pericardium_spring=pericardium_spring,
+            base_spring=base_spring
+        )
+    )
+    geometry = pulse.HeartGeometry.from_file(
+        (out_dirs['unload'] / 'unloaded_geometry_0_with_fibers.h5').as_posix(), comm=comm
+    )
+    model = HeartModelDynaComp(
+        geo=geometry,
+        bc_params=bc_params,
+        matparams=settings['matparams'],
+        comm=comm,
+    )
+
+    # Set up data collector with live plotting
+    collector = DataCollectorInflator(
+        outdir=out_dirs['model'],
+        problem=model,
+        pv_vols=vols,
+        pv_pres=pres,
+        edpvr_vols=edpvr_vols,
+        edpvr_pres=edpvr_pres,
+        live_plot=live_plot
+    )
+
+    # Run inflation steps
+    v0 = model.compute_volume(activation_value=0, pressure_value=0)
+    collector.collect(time=0, pressure=0, volume=v0)
+
+    pressures = np.linspace(0, pres[0] * pressure_multiplier, pressure_steps)
+    for i, p in enumerate(pressures, start=1):
+        model.compute_volume(activation_value=0, pressure_value=p)
+        collector.collect(time=i, pressure=model.get_pressure(), volume=model.get_volume())
+
+    if comm.rank == 0:
+        collector.finalize_plot()
+
+    return collector
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -91,74 +184,26 @@ def main():
     )
     args = parser.parse_args()
 
-    # Determine which samples to process
+    # Determine sample list
     if args.number:
         sample_list = args.number
     else:
         settings_files = sorted([f for f in args.settings_dir.iterdir() if f.suffix == ".json"])
         sample_list = list(range(1, len(settings_files) + 1))
 
-    bc_params = arg_parser.create_bc_params(args)
-    comm = dolfin.MPI.comm_world
-
+    # Run inflator for each sample with explicit params
     for sample in sample_list:
-        settings = load_settings(args.settings_dir, sample)
-        name = settings['id']
-        out_dirs = {
-            'pv': args.results_dir / name / 'PV Data',
-            'calib': args.results_dir / name / args.scan_type / '01_PVCalibration',
-            'unload': args.results_dir / name / args.scan_type / '02_Unloading',
-            'model': args.results_dir / name / args.scan_type / '03_Modeling'
-        }
-        if comm.rank == 0:
-            arg_parser.prepare_oudir_processing(out_dirs['model'], comm)
-        comm.Barrier()
-
-        # Load data
-        time, pres, vols = load_pv_data(out_dirs['calib'])
-        edpvr_pres, edpvr_vols_unc = load_edpvr(out_dirs['pv'])
-        a = settings['PV']['calibration']['a']
-        b = settings['PV']['calibration']['b']
-        edpvr_vols = a * edpvr_vols_unc + b
-
-        # Set up model
-        geometry = pulse.HeartGeometry.from_file(
-            (out_dirs['unload'] / 'unloaded_geometry_0_with_fibers.h5').as_posix(), comm=comm
-        )
-        model = HeartModelDynaComp(
-            geo=geometry,
-            bc_params=bc_params,
-            matparams=settings['matparams'],
-            comm=comm,
-        )
-
-        # Initialize data collector with live plotting
-        collector = DataCollectorInflator(
-            outdir=out_dirs['model'],
-            problem=model,
-            pv_vols=vols,
-            pv_pres=pres,
-            edpvr_vols=edpvr_vols,
-            edpvr_pres=edpvr_pres,
+        run_inflator(
+            sample,
+            settings_dir=args.settings_dir,
+            results_dir=args.results_dir,
+            scan_type=args.scan_type,
+            pressure_multiplier=args.pressure_multiplier,
+            pressure_steps=args.pressure_steps,
+            pericardium_spring=args.pericardium_spring,
+            base_spring=args.base_spring,
             live_plot=True
         )
-
-        # Initial state
-        v0 = model.compute_volume(activation_value=0, pressure_value=0)
-        collector.collect(time=0, pressure=0, volume=v0)
-
-        # Simulation loop
-        pressures = np.linspace(0, pres[0] * args.pressure_multiplier, args.pressure_steps)
-        for i, p in enumerate(pressures, start=1):
-            model.compute_volume(activation_value=0, pressure_value=p)
-            collector.collect(time=i, pressure=model.get_pressure(), volume=model.get_volume())
-
-        # Finalize plot session
-        if comm.rank == 0:
-            collector.finalize_plot()
-
-    return 0
-
 
 if __name__ == '__main__':
     main()
