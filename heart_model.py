e2a48705d4fb4cc1764a585a9bd1adcb5ac054ba
mjsadeghinia <mjsadeghinia@outlook.com>
2025-05-15 19:27:51 +0000

    added update_matparams for material parameter update

--- heart_model.py
+++ heart_model.py
@@ -435,6 +435,24 @@ class HeartModelDynaComp:
             s0=self.geometry.s0,
             n0=self.geometry.n0,
         )
+    
+    def update_matparams(self, matparams):
+        """
+        Updates the material parameters of the heart model.
+        Parameters:
+        matparams (dict): Dictionary of material parameters to be updated.
+        """
+        # Use provided fiber_angles or default ones if not provided
+        for name, val in matparams.items():
+            if not hasattr(self.material, name):
+                raise ValueError(f"Invalid material parameter: {name}")
+            # Update the material parameters
+            getattr(self.material, name).assign(val)
+            getattr(self.problem.material, name).assign(val)
+
+        # Update the material parameters in the problem
+        self.material = self.get_material_model(matparams)
+        self.problem.material = self.get_material_model(matparams)
 
     def get_matparams(self, matparams: dict = dict()):
         # Use provided fiber_angles or default ones if not provided
