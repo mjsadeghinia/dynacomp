687575f7c97f089a03485396592374a78446e95c
mjsadeghinia <mjsadeghinia@outlook.com>
2025-10-14 10:43:47 +0000

    updated utils

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import json
 from pathlib import Path
 import numpy as np
 
+from structlog import get_logger
+
+logger = get_logger()
 
 def load_settings(settings_dir: Path, sample_num: int) -> dict:
     files = sorted([f for f in settings_dir.iterdir() if f.is_file() and f.suffix == ".json"])
@@ -29,9 +32,18 @@ def load_pressure_volumes(pv_dir):
 def read_edpvr_data(edpvr_dir):
     fname = edpvr_dir / "inflation_results.txt"
     if not fname.exists():
-        raise FileNotFoundError(f"EDPVR data file {fname} does not exist.")
+        logger.error(FileNotFoundError(f"EDPVR data file {fname} does not exist."))
+        return None
     edpvr_data = np.loadtxt(fname, delimiter=',', skiprows=1)
     return edpvr_data
+
+def read_fiber_data(fiber_dir):
+    fname = fiber_dir / "Fiber_results.csv"
+    if not fname.exists():
+        logger.error(FileNotFoundError(f"Fiber data file {fname} does not exist."))
+        return None
+    fiber_data = np.loadtxt(fname, delimiter=',', skiprows=1)
+    return fiber_data
 
 def get_folders_from_edpvr_data(edpvr_data):
     edpvr_data_sorted = edpvr_data[edpvr_data[:, -1].argsort()]
